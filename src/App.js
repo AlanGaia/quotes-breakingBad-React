366d7d2c1c39a63b5d688052311704fd052ba78c
Alan <alan.j.gaia@gmail.com>
2020-12-10 15:45:48 -0300

    initial commit - doing Fetch API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  cursor: pointer; 
+
+  :hover{
+  background: -webkit-linear-gradient(top left, #09b350 0%, #09b350 40%, #1ca393 100%);
+  }
+`;
+
 
 function App() {
+
+  const requestAPI = () => {
+    const result = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    console.log(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button onClick={requestAPI}>Get Phrase</Button>
+    </Container>
   );
 }
 
